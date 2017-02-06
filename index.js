36c0b7b1a2193e2694fc6cfee97998e1d7b07ea0
Dustin Rue <ruedu@dustinrue.com>
2017-02-06 09:21:37 -0600

    Dev (#1)
    * try an interval
    
    * ping test
    
    * ping test
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * attempt to detect if the xbox turned on properly
    
    * longer delay before returning success/failure on power toggle
    
    * the delays were reversed
    
    * return success immediately as we can't really determine power state reliably
    
    * stop the ping probe for awhile after power toggle
    
    * immediately set power state on power toggle
    
    * report power state via logs
    
    * report power state via logs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var ping = require('ping');
 var lirc = require('lirc_node');
+var powerState = 0;
+var pingTimer = null;
+var switchService;
 
 var Service, Characteristic;
 
@@ -15,30 +18,65 @@ function XboxAccessory(log, config) {
   this.ip = config['ipAddress'];
 }
 
+function pinger(switchService, xboxAccessory) {
+  var self = xboxAccessory;
+
+  ping.sys.probe(self.ip, function(isAlive) {
+    powerState = isAlive;
+    if (isAlive) {
+      switchService.getCharacteristic(Characteristic.On).getValue();
+    }
+    else {
+      switchService.getCharacteristic(Characteristic.On).getValue();
+    }
+  });
+}
+
+function startPinger(switchService, xboxAccessory) {
+  pingTimer = setInterval(function() {
+      pinger(switchService, xboxAccessory);
+    }, 1000 * 5);
+}
+
 XboxAccessory.prototype = {
 
   setPowerState: function(powerOn, callback) {
     var self = this;
-    
+
+    // stop updating power status for awhile
+    clearInterval(pingTimer);
+
     if (powerOn) {
+      powerState = 1;
       lirc.irsend.send_once('XBOX-ONE', 'PowerOn', function() {
-        self.log("Sending power on command to '" + this.name + "'...");
+        self.log("Sending power on command to '" + self.name + "'...");
       });
+      setTimeout(function() {
+        startPinger(switchService, self);
+      }, 150000);
     }
     else {
+      powerState = 0;
       lirc.irsend.send_once('XBOX-ONE', 'PowerOff', function() {
-        self.log("Sending power off command to '" + this.name + "'...");
+        self.log("Sending power off command to '" + self.name + "'...");
       });
+      setTimeout(function() {
+        startPinger(switchService, self);
+      }, 150000);
     }
-
-    // Don't really care about powerOn errors, and don't want more than one callback
+    
+    // we can't reliably determine if the Xbox has heard us.
+    // The delay between power on and network availability depends on 
+    // if the user is using power save mode vs instant on. 
+    // Power off doesn't down the network interface immediately and
+    // the Xbox often brings up the interface to check for updates
     callback();
   },
 
   getPowerState: function(callback) {
-    ping.sys.probe(this.ip, function(isAlive){
-      callback(null, isAlive);
-    });
+    var status = (powerState) ? "up":"down"
+    this.log("is " + status);
+    callback(null, powerState);
   },
 
   identify: function(callback) {
@@ -47,7 +85,7 @@ XboxAccessory.prototype = {
   },
 
   getServices: function() {
-    var switchService = new Service.Switch(this.name);
+    switchService = new Service.Switch(this.name);
 
     switchService
       .getCharacteristic(Characteristic.On)
@@ -56,6 +94,13 @@ XboxAccessory.prototype = {
     switchService
       .getCharacteristic(Characteristic.On)
       .on('get', this.getPowerState.bind(this));
+
+    var xboxAccessory = this;
+
+    // setup a timer to check the power state of the Xbox
+    // This isn't terribly reliable for reasons listed above
+    // but is better than nothing
+    startPinger(switchService, xboxAccessory);
 
     return [switchService];
   }
